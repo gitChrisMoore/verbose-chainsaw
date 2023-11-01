38f29ae994170d95083810545d3dd8a49b0609f0
Chris Moore <chrisdmoore06@gmail.com>
2023-11-01 11:42:41 -0400

    basic api

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, request
-from .services.openai_service import get_user_story_data_analysis
+from flask import Flask, jsonify, render_template, request
+from .services.openai_service import get_user_story_data_analysis, get_story_ui_analysis
 
 app = Flask(__name__)
 
 
+@app.route("/api", methods=["GET"])
+def get_data():
+    data = {"message": "Hello from Flask!", "items": ["item1", "item2", "item3"]}
+    return jsonify(data), 200
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    response_text = ""
+    res_data_analysis = ""
+    res_ui_analysis = ""
     if request.method == "POST":
         user_story = request.form.get("user_story")
 
         # Check if user_story is a string and its length is greater than 3
         if isinstance(user_story, str) and len(user_story) > 3:
-            response_text = get_user_story_data_analysis(user_story)
+            res_data_analysis = get_user_story_data_analysis(user_story)
+            res_ui_analysis = get_story_ui_analysis(user_story)
 
-    return render_template("hello.html", response_text=response_text)
+    return render_template(
+        "hello.html",
+        res_data_analysis=res_data_analysis,
+        res_ui_analysis=res_ui_analysis,
+    )
--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from app.utils import configuration
 
 
@@ -40,18 +41,98 @@ def get_openai_response(messages):
         return ""
 
 
+def parse_score(raw_string):
+    """
+    Parses the score from the OpenAI response.
+    """
+    pattern = r"__@score__(.*?)__@endscore__"
+    match = re.search(pattern, raw_string)
+    if match:
+        print(match.group(1))
+        return match.group(1)
+    else:
+        return 0
+
+
+def parse_suggestions(raw_string):
+    """
+    Parses the suggestions from the OpenAI response.
+    """
+    content = raw_string.split("###recs_b")[1].split("###recs_e")[0].strip()
+    entries = [entry.strip() for entry in content.split("####") if entry.strip()]
+    if entries:
+        print(entries)
+        return entries
+    else:
+        return []
+
+
 def get_user_story_data_analysis(user_story):
     """
     Returns the response from OpenAI API for the given user story.
     """
+
+    res = {
+        "score": 0,
+        "suggestions": [],
+    }
     previous_messages = [
         {
             "role": "system",
-            "content": "You are a data systems analyst responsible for evaluating user story complete and correctness.\n\nStart at 0, and add points only if one of these conditions is observed:\n- It must provide specific data fields and each of the fields formats\n\nDo not grant any score for the base user story.  All additional notes must come after the user story.\n\nRespond with the score and a list of suggested improvements",
+            "content": "You are a data systems analyst responsible for evaluating user story complete and correctness.\n\nStart at 0, and add points only if one of these conditions is observed:\n- It must provide specific data fields and each of the fields formats\n\nDo not grant any score for the base user story.  All additional notes must come after the user story.\n\nRespond only with the SCORE and list of SUGGESTIONS contextual to the user story.\nSUGGESTIONS should be a list of no more than 5 concise statements that should not contain explanations.\nThe SCORE should be located between ###score_b and ###score_e. The SCORE should be a number between 0 and 100.\nThe SUGGESTIONS should be located between ###recs_b and ###recs_e, each starting with ####.",
+        },
+        {
+            "role": "user",
+            "content": "As a Covered Member, I want to submit a short term disability claim.",
+        },
+        {
+            "role": "assistant",
+            "content": "###score_b\n0\n###score_e\n\n###recs_b\n#### Specify the data fields required for submitting a short-term disability claim.\n#### Define the formats for each of these data fields.\n#### Determine if there are any conditions or rules associated with the submission of the short term disability claim.\n#### The user story could be more detailed in terms of processes and expected outcomes.\n#### It is recommended to clarify how the process of claim submission should be implemented or automated for the covered member.\n###recs_e",
         },
         {
             "role": "user",
             "content": user_story,
         },
     ]
-    return get_openai_response(previous_messages)
+
+    openai_res = get_openai_response(previous_messages)
+    res["score"] = parse_score(openai_res)
+    res["suggestions"] = parse_suggestions(openai_res)
+
+    return res
+
+
+def get_story_ui_analysis(user_story):
+    """
+    Returns the response from OpenAI API for the given user story.
+    """
+
+    res = {
+        "score": 0,
+        "suggestions": [],
+    }
+
+    previous_messages = [
+        {
+            "role": "system",
+            "content": "As a UI analyst, your role is to evaluate the completeness and correctness of a user story. Please use the following criteria for your evaluation:\n1. Document main, alternative, and exit flows for the user\n2. Specify each UI component'''s type and whether to reuse it from an existing design system.\n3. Provide layout guidelines, including responsiveness for different screen sizes.\n4. Highlight accessibility standards, brand, and style guidelines\n\n\nRespond only with the SCORE and list of SUGGESTIONS contextual to the user story.\n\nSUGGESTIONS should be a list of no more than 5 concise statements that should not contain explinations.\n\nThe SCORE should be located between ###score_b and ###score_e. The SCORE should be a number between 0 and 100.\nThe SUGGESTIONS should be located between ###recs_b and ###recs_e, each starting with ####.\n",
+        },
+        {
+            "role": "user",
+            "content": "As a Covered Member, I want to submit a short term disability claim.",
+        },
+        {
+            "role": "assistant",
+            "content": "###score_b 5 ###score_e\n\n###recs_b\n##Document the main, alternative, and exit path of the user\n##Specify the type of each UI component\n##Provide layout and responsiveness guidelines\n###recs_e",
+        },
+        {
+            "role": "user",
+            "content": user_story,
+        },
+    ]
+
+    openai_res = get_openai_response(previous_messages)
+    res["score"] = parse_score(openai_res)
+    res["suggestions"] = parse_suggestions(openai_res)
+
+    return res
